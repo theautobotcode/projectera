bb829dfe63f08cf5221096603395fe89c434542c
theautobotcode <theautobot@proton.me>
2025-01-06 16:42:16 +0530

    swagger added

--- projectera/urls.py
+++ projectera/urls.py
@@ -21,6 +21,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
+    path('api/accounts/',include('accounts.api_urls')),
     path('',include('userview.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
