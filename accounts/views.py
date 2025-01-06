0cf5c2baabf05006c2ef13c38fae772efdba924f
theautobotcode <theautobot@proton.me>
2025-01-06 16:44:26 +0530

    issue fixed

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,7 @@ class LoginView(GenericAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         data = serializer.data
-        user = authenticate(request,username=data['username'],password=data['password'])
+        user = authenticate(request,username=data['email'],password=data['password'])
         if user:
             login(request,user)
             refresh = RefreshToken.for_user(user)
